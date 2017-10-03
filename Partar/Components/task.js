d4887e1566ac375d292e3b481ed0484d7a5b36e3
Jack Daniels <jakob-daniel@hotmail.com>
2017-10-03 10:06:35 +0000

    Components Folder Added

---
+++ Partar/Components/task.js
@@ -0,0 +1,32 @@
+Vue.component("task-list", {
+	template: `
+		<ul>
+			<task v-for='task in tasks' v-text='task.title'>
+
+			</task>
+		</ul>`
+		,
+
+	data: function(){
+		return {
+			tasks: [
+				{ title: "Fara í búðina", complete: true},
+				{ title: "Skoða heiminn", complete : false},
+				{ title: "Pota í lávaxna karlmenn", complete: true},
+				{ title: "Nota lyklaborð sem innstungu", complete: false},
+			]
+		}
+	}
+})
+
+Vue.component("task", {
+	template: "<li><slot></slot></li>"
+})
+
+
+var app = new Vue({
+	el: "#app",
+	data: {
+
+	}
+})
